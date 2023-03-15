d2e33b23808c23bc195de7fc78a40f85fc26ba49
GuillaumeZhu <80273907+GuillaumeZhu@users.noreply.github.com>
2023-03-15 19:26:35 +0100

    feat(shop): scrape new products

--- server/eshops/dedicatedbrand.js
+++ server/eshops/dedicatedbrand.js
@@ -2,44 +2,98 @@ const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
 /**
- * Parse webpage e-shop
- * @param  {String} data - html response
- * @return {Array} products
+ * Extract the sub-page URLs to be scraped from the main navigation menu
+ * @param  {String} data - HTML response
+ * @return {Array} Array of URL objects
  */
-const parse = data => {
+const extractUrls = (data) => {
   const $ = cheerio.load(data);
-
-  return $('.productList-container .productList')
+  return $('.mainNavigation-fixedContainer .mainNavigation-link-subMenu-link')
     .map((i, element) => {
-      const name = $(element)
-        .find('.productList-title')
-        .text()
-        .trim()
-        .replace(/\s/g, ' ');
-      const price = parseInt(
-        $(element)
-          .find('.productList-price')
-          .text()
-      );
-
-      return {name, price};
+      const url = 'https://www.dedicatedbrand.com' + 
+      $(element)
+      .children('a')
+      .attr('href');
+      
+      return { url };
     })
     .get();
 };
 
 /**
- * Scrape all the products for a given url page
- * @param  {[type]}  url
- * @return {Array|null}
+ * Extract product information from a single product list element
+ * @param  {Object} $ - Cheerio instance
+ * @param  {Object} element - Product list element
+ * @return {Object} Product object
  */
-module.exports.scrape = async url => {
+const extractProduct = ($, element) => {
+  const name = $(element)
+    .find('.productList-title')
+    .text()
+    .trim()
+    .replace(/\s/g, ' ');
+  const price = parseInt(
+    $(element)
+      .find('.productList-price')
+      .text()
+  );
+  const link = 'https://www.dedicatedbrand.com' + $(element)
+    .find('.productList-link')
+    .attr('href');
+  const brand = 'Dedicated';
+  const image = $(element)
+    .find('.productList-image')
+    .children('img')
+    .attr('src');
+  return { name, price, link, brand, image };
+};
+
+/**
+ * Scrape all products from a single page
+ * @param  {String} url - URL of page to scrape
+ * @return {Array} Array of product objects
+ */
+const scrapePage = async (url) => {
   try {
     const response = await fetch(url);
 
     if (response.ok) {
       const body = await response.text();
+      const $ = cheerio.load(body);
+      return $('.productList-container .productList')
+        .map((i, element) => extractProduct($, element))
+        .get();
+    }
 
-      return parse(body);
+    console.error(response);
+
+    return null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+/**
+ * Scrape all products from all pages
+ * @param  {String} url - URL of main page to start from
+ * @return {Array|null} Array of all product objects, or null if an error occurred
+ */
+module.exports.scrape = async (url) => {
+  try {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const body = await response.text();
+      const urlsToScrape = extractUrls(body);
+      let allProducts = [];
+
+      for (const urlObj of urlsToScrape) {
+        const products = await scrapePage(urlObj.url);
+        allProducts.push(...products);
+      }
+
+      return allProducts;
     }
 
     console.error(response);
--- server/sandbox.js
+++ server/sandbox.js
@@ -1,15 +1,66 @@
 /* eslint-disable no-console, no-process-exit */
-const dedicatedbrand = require('./eshops/dedicatedbrand');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+
+//Import required modules
+const dedicatedbrand = require('./eshops/dedicatedbrand');
+const Montlimartbrand= require('./eshops/Montlimartbrand');
+const circlesportswearbrand=require('./eshops/circlesportswearbrand')
+const fs =require('fs');
+
+
+//Define URLs for Montlimart and Circle Sportswear
+const url_montlimart=["https://www.montlimart.com/99-vetements",
+                      "https://www.montlimart.com/14-chaussures",
+                      "https://www.montlimart.com/15-accessoires"];
+
+const url_circlesportswear=["https://shop.circlesportswear.com/collections/collection-femme",
+                            "https://shop.circlesportswear.com/collections/collection-homme"];
+
+
+// Define an empty array for storing the all products                        
+let final=[]
+
+//Define a sandbox function to scrape all products
+async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/') {
   try {
+
     console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
 
-    const products = await dedicatedbrand.scrape(eshop);
+   //Scrape products for Circle Sportswear
+    for(url in url_circlesportswear){
+      const products = await circlesportswearbrand.scrape(url_circlesportswear[url]) ;
+      final=final.concat(products);
+    }
 
-    console.log(products);
-    console.log('done');
+    //Scrape products for Circle Sportswear
+    const number_product_circle=final.length;
+    
+    //Get number of products scraped from Montlimart
+    for(url in url_montlimart){
+      const products = await Montlimartbrand.scrape(url_montlimart[url]);
+      final=final.concat(products)
+    }
+
+    //Get number of products scraped from Montlimart
+    const number_product_montlimart=final.length-number_product_circle;
+
+    //Scrape products for Dedicated Brand
+    const products = await dedicatedbrand.scrape(eshop);
+    final=final.concat(products)
+   
+    //Log the final array of products
+    console.log(final);
+    console.log("Number of products on circle sportswear :",number_product_circle);
+    console.log("Number of products on montlimart :",number_product_montlimart);
+    console.log("Number of products on dedicated :",final.length-number_product_montlimart)
+    console.log("Total number of products =",final.length);
+
+    // Write the final result to a JSON file
+    fs.writeFileSync('TotalProduct.json', JSON.stringify(final, null, 2));
+
+    //console.log('done');
     process.exit(0);
+    
   } catch (e) {
     console.error(e);
     process.exit(1);
