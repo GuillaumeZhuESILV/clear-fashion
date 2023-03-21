45eab2dc7df6da611239af21367584722778fd6c
GuillaumeZhu <80273907+GuillaumeZhu@users.noreply.github.com>
2023-03-21 19:19:54 +0100

    feat(get-product): get a specific product

--- server/MongoDB.js
+++ server/MongoDB.js
@@ -1,11 +1,10 @@
 const products=require('./TotalProduct.json');
-
+const {MongoClient} = require('mongodb');
+const MONGODB_URI = "mongodb+srv://guillaumezhu6:WoS82Qy78hOFIeF1@cluster0.e8sr63z.mongodb.net/test";
+const MONGODB_DB_NAME = 'clearfashion';
 async function connectToDatabase() {
     try {
-      const {MongoClient} = require('mongodb');
-      const MONGODB_URI = "mongodb+srv://guillaumezhu6:WoS82Qy78hOFIeF1@cluster0.e8sr63z.mongodb.net/test";
-    
-      const MONGODB_DB_NAME = 'clearfashion';
+      
       const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
       const db = client.db(MONGODB_DB_NAME);
       console.log("Connected to the database")
@@ -13,7 +12,7 @@ async function connectToDatabase() {
       collection.drop();
       const result = await collection.insertMany(products);
       //console.log(result);
-
+      
       //Search all the products from Montlimart
       const brand = 'Montlimart';
       console.log("search for Montlimart");
@@ -22,23 +21,23 @@ async function connectToDatabase() {
 
       //Search all the products less than a price
       price=50
-      const results = await collection.find({ brand: 'Montlimart', price: { $lt: 50 } }).toArray();
+      const results = await collection.find({ price: { $lt: 50 } }).toArray();
       //console.log(results);
       
       //Ascending
       const sorted_result_asc= await collection.find().sort({price:1}).toArray();
       //Descending
       const sorted_result_desc= await collection.find().sort({price:-1}).toArray();
-
       //console.log(sorted_result_asc);
 
       //Find all products sorted by date
       const sorted_date_result= await collection.find().sort({date: 1}).toArray();
-      console.table(sorted_date_result);
+      //console.table(sorted_date_result);
 
 
       //Find all products scraped less than 2 weeks
-      const less_2weeks_products=db.products.find({date: {$lt: new Date(Date.now() - (14 * 24 * 60 * 60 * 1000))}});
+
+      const less_2weeks_products=await collection.find({date: {$lt: new Date(Date.now() - (14 * 24 * 60 * 60 * 1000))}}).toArray();
       //console.log(less_2weeks_products);
 
       client.close();
@@ -48,4 +47,7 @@ async function connectToDatabase() {
     }
   }
 
-  connectToDatabase();
+ connectToDatabase();
+
+
+
--- server/api.js
+++ server/api.js
@@ -3,7 +3,10 @@ const express = require('express');
 const helmet = require('helmet');
 
 const PORT = 8092;
-
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const CONNECTION_URL="mongodb+srv://guillaumezhu6:WoS82Qy78hOFIeF1@cluster0.e8sr63z.mongodb.net/test";
+const DATABASE_NAME = 'clearfashion';
 const app = express();
 
 module.exports = app;
@@ -18,6 +21,71 @@ app.get('/', (request, response) => {
   response.send({'ack': true});
 });
 
-app.listen(PORT);
+
+
+
+app.get("/products", (request, response) => {
+  collection.find({}).toArray((error, result) => {
+      if(error) {
+          return response.status(500).send(error);
+      }
+      response.send(result);
+  });
+});
+
+app.get("/products/:id", (request, response) => {
+  collection.findOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
+      if(error) {
+          return response.status(500).send(error);
+      }
+      response.send(result);
+  });
+});
+
+
+
+app.get("/products/search", (req, res) => {
+  const limit = parseInt(req.query.limit) || 12;
+  const brand = req.query.brand || "";
+  const price = parseInt(req.query.price) || 0;
+
+  const query = {};
+
+  if (brand !== "") {
+    query.brand = brand;
+  }
+
+  if (price !== 0) {
+    query.price = { $lte: price };
+  }
+
+  collection
+    .find(query)
+    .limit(limit)
+    .sort({ price: 1 })
+    .toArray((err, result) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      const total = result.length;
+      res.send({ limit, total, results: result });
+    });
+});
+
+
+
+
+
+
+app.listen(PORT, () => {
+  MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+      if(error) {
+          throw error;
+      }
+      database = client.db(DATABASE_NAME);
+      collection = database.collection("products");
+      console.log("Connected to `" + DATABASE_NAME + "`!");
+  })});
+
 
 console.log(`📡 Running on port ${PORT}`);
